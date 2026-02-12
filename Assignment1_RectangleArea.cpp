93847b6b9dfda15cc5612f6610e9db715003352a
Saavedra, Marc <marcsaave@gmail.com>
2026-02-11 17:42:10 -0800

    Rectangle area calc

--- Assignment1_RectangleArea.cpp
+++ Assignment1_RectangleArea.cpp
@@ -11,22 +11,23 @@ int main()
 	double height;
 	double width;
 	cout << "Enter height and width: ";
-/* write your code here */
+	/* write your code here */
+	cin >> height; 
+	cin >> width;
 	// calculate area
-	
+	double area = height * width ;
 
 
-
-	return 0;
 
 	// write output to console
-	
 
+	cout << "Area is " << area << endl;
 
 
 
 
 	// return value that indicates normal program exit
+	cout << "Area has been calculated" << endl;
     return 0;
 }
 
