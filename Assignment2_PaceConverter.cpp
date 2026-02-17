79c98e42a7781837f6a26abe7704865e77be2902
Saavedra, Marc <marcsaave@gmail.com>
2026-02-17 12:58:06 -0800

    Assignment2_PaceConverter.cpp

--- Assignment2_PaceConverter.cpp
+++ Assignment2_PaceConverter.cpp
@@ -3,13 +3,23 @@
 using namespace std;
 int main()
 {
-    double mph = 6.5;
+    double mph;
     // PACE calculation
     /* write your code */
+    cout << "Enter your MPH speed!\n";
+    cin >> mph;
+    double pace;
+    pace = 60.0/mph;
+    int minutes = pace;
+    double seconds = (pace - minutes) * 60;
 
     //express pace in minutes and seconds
   /* write your code here */
-
+  
+    cout << "Your pace is "
+       << minutes<< " minutes and "
+       <<seconds<< " seconds per mile."
+       << endl;
   //express your output in 1 decimal points
 
 
